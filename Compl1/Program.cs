e05208615ad8ea4a5396a4ef0114d2038e700f52
Smak2025 <smak-80+2025@yandex.ru>
2025-09-10 09:51:32 +0300

    Добавьте файлы проекта.

---
+++ Compl1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Numerics;
+using System.Text;
+
+Complex z1 = new Complex() { Re = 2, Im = 3 };
+Console.WriteLine(z1);
+Complex z2 = new Complex() { Im = 3 };
+Console.WriteLine(z2);
+Complex z3 = new Complex() { Im = -3 };
+Console.WriteLine(z3);
+Complex z4 = new Complex() { Im = -1 };
+Console.WriteLine(z4);
+Complex z5 = new Complex() {Re = -1};
+Console.WriteLine(z5);
+Complex z6 = new Complex();
+Console.WriteLine(z6);
+
+Complex z7 = z5 + z3;
+Console.WriteLine(z7);
+class Complex
+{
+    private double re;
+    private double im;
+    public double Re
+    { get { return re; } set { re = value; } }
+    public double Im
+    {
+        get => im;
+        set => im = value;
+    }
+    public double Abs => Math.Sqrt(re*re + im*im);
+
+    public static Complex operator +(Complex z1, Complex z2)
+    {
+        return new Complex() { Re = z1.re + z2.Re, Im = z1.im + z2.Im };
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        if (re != 0 || im == 0) sb.Append(re);
+        if (im != 0)
+        {
+            if (im > 0) { 
+                if (re != 0)
+                    sb.Append("+");
+            }
+            else sb.Append("-");
+            if (im != 1 && im != -1) sb.Append(Math.Abs(im));
+            sb.Append("i");
+        }
+        return sb.ToString();
+    }
+
+}
